c8368370b7249a6d798af682294ab8ceeeb6cba1
Khaled Ramadan <khaledramadan@fadis-MacBook-Pro.local>
2019-12-15 18:09:01 +0200

    Installed the native gealocation plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
-import { NoDataComponent } from './components/no-data/no-data.component';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,8 +19,9 @@ import { NoDataComponent } from './components/no-data/no-data.component';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
